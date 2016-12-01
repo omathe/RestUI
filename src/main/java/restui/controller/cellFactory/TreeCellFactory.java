451cd3fd378922cb61a4baaf5ab960eea09ec805
Olivier MATHE <omathe@free.fr>
2016-12-01 22:37:26 +0100

    feat(Item) : move an item

--- src/main/java/restui/controller/cellFactory/TreeCellFactory.java
+++ src/main/java/restui/controller/cellFactory/TreeCellFactory.java
@@ -6,9 +6,14 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.TextFieldTreeCell;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.TransferMode;
 import restui.model.Endpoint;
 import restui.model.Item;
 import restui.model.Path;
@@ -16,18 +21,20 @@ import restui.model.Project;
 
 public class TreeCellFactory extends TextFieldTreeCell<Item> {
 
+	private TreeView<Item> treeView;
 	private TextField textField;
 	private final ContextMenu addMenu = new ContextMenu();
 	private final MenuItem menuItemPath;
-	private final MenuItem menuItemWs;
+	private final MenuItem menuItemEndpoint;
+	private final MenuItem menuDeleteItem;
+	private TreeItem<Item> treeItemToMove;
 
 	@SuppressWarnings("unchecked")
-	public TreeCellFactory() {
+	public TreeCellFactory(final TreeView<Item> treeView) {
 
-		menuItemPath = new MenuItem("Nouveau chemin");
-		menuItemWs = new MenuItem("Nouveau ws");
-		// addMenu.getItems().add(menuItemPath);
-		// addMenu.getItems().add(menuItemWs);
+		menuItemPath = new MenuItem("New path");
+		menuItemEndpoint = new MenuItem("New endpoint");
+		menuDeleteItem = new MenuItem("Delete");
 
 		menuItemPath.setOnAction(new EventHandler() {
 			@Override
@@ -36,26 +43,135 @@ public class TreeCellFactory extends TextFieldTreeCell<Item> {
 				final Path path = new Path(item, "path");
 				item.addChild(path);
 				final TreeItem<Item> newItem = new TreeItem<>(path);
-				
+
 				getTreeItem().getChildren().add(newItem);
 			}
 		});
 
-		menuItemWs.setOnAction(new EventHandler() {
+		menuItemEndpoint.setOnAction(new EventHandler() {
 			@Override
 			public void handle(final Event t) {
 				final Item item = getTreeItem().getValue();
 				final Endpoint endPoint = new Endpoint(item, "new endpoint", "GET");
 				item.addChild(endPoint);
-				
-				// exchanges
-				//final Exchange ex1 = new Exchange("e1", Instant.now().toEpochMilli());
-				//final Exchange ex2 = new Exchange("e2", Instant.now().toEpochMilli());
-				//endPoint.addExchange(ex1);
-				//endPoint.addExchange(ex2);
-				
 				final TreeItem<Item> newItem = new TreeItem<>(endPoint);
 				getTreeItem().getChildren().add(newItem);
+			}
+		});
+
+		menuDeleteItem.setOnAction(new EventHandler() {
+			@Override
+			public void handle(final Event t) {
+				final TreeItem<Item> itemToDelete = getTreeItem();
+				if (itemToDelete.getParent() != null) {
+					getTreeItem().getParent().getChildren().remove(getTreeItem());
+				} else {
+					treeView.setRoot(null);
+				}
+			}
+		});
+
+		// Drag and drop management
+		
+		// (1)
+		setOnDragDetected(e -> {
+			final Item item = getItem();
+			System.out.println("DragDetected " + item);
+
+			final Dragboard db = startDragAndDrop(TransferMode.MOVE);
+	        
+	        /* Put a string on a dragboard */
+	        final ClipboardContent content = new ClipboardContent();
+	        content.putString("toto");
+	        
+	        db.setContent(content);
+	        
+	        treeItemToMove = getTreeItem();
+	        System.out.println("treeItemToMove() " + treeItemToMove);
+            
+			e.consume();
+
+		});
+
+		// (2)
+		setOnDragOver(new EventHandler<DragEvent>() {
+
+			@Override
+			public void handle(final DragEvent event) {
+				if (event.getGestureSource() != event.getTarget()) {
+					/*
+					 * allow for both copying and moving, whatever user chooses
+					 */
+					event.acceptTransferModes(TransferMode.MOVE);
+					final Item item = getItem();
+
+					// System.out.println("over " + item);
+					// System.out.println("getTreeItem() " + getTreeItem());
+					//System.out.println("DragOver " + event.getTarget());
+					// item.setName(event.getDragboard().getString());
+				}
+				event.consume();
+			}
+		});
+
+		// (3)
+		setOnDragEntered(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(final DragEvent event) {
+
+				// System.out.println("DragEntered " + event.getTarget());
+				System.out.println("DragEntered() " + getTreeItem());
+
+				/* the drag-and-drop gesture entered the target */
+				/* show to the user that it is an actual gesture target */
+
+				// target = event.getTarget();
+				// if (event.getGestureSource() != event.getTarget() &&
+				// event.getDragboard().hasString()) {
+				// event.getTarget().setFill(Color.GREEN);
+				// }
+
+				event.consume();
+			}
+		});
+
+		// (4)
+		setOnDragExited(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(final DragEvent event) {
+				/* mouse moved away, remove the graphical cues */
+				System.out.println("DragExited " + event.getTarget());
+
+				event.consume();
+			}
+		});
+
+		// (5)
+		setOnDragDropped(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(final DragEvent event) {
+				System.out.println("DragDropped " + getTreeItem());
+//				System.out.println("DragDropped source" + event.getSource());
+				final Object obj = event.getSource();
+//				System.out.println("DragDropped event.getGestureSource()" + event.getGestureSource());
+				/*
+				 * let the source know whether the string was successfully
+				 * transferred and used
+				 */
+				event.setDropCompleted(true);
+
+				event.consume();
+			}
+		});
+
+		// (6)
+		setOnDragDone(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(final DragEvent event) {
+				/* the drag and drop gesture ended */
+				/* if the data was successfully moved, clear it */
+				System.out.println("DragDone " + event.getTarget());
+				event.consume();
 			}
 		});
 	}
@@ -80,14 +196,20 @@ public class TreeCellFactory extends TextFieldTreeCell<Item> {
 				setContextMenu(addMenu);
 
 				if (item instanceof Project || item instanceof Path) {
-					setContextMenu(addMenu);
 					addMenu.getItems().add(menuItemPath);
-					addMenu.getItems().add(menuItemWs);
+					addMenu.getItems().add(menuItemEndpoint);
+					addMenu.getItems().add(menuDeleteItem);
+				}
+				else if (item instanceof Endpoint) {
+					if (!addMenu.getItems().contains(menuDeleteItem)) {
+						addMenu.getItems().add(menuDeleteItem);
+					}
 				}
 
-//				if (!getTreeItem().isLeaf() && getTreeItem().getParent() != null) {
-//					setContextMenu(addMenu);
-//				}
+				// if (!getTreeItem().isLeaf() && getTreeItem().getParent() !=
+				// null) {
+				// setContextMenu(addMenu);
+				// }
 			}
 		}
 	}
@@ -114,8 +236,9 @@ public class TreeCellFactory extends TextFieldTreeCell<Item> {
 				if (e.getCode() == KeyCode.ENTER) {
 					getItem().setName(textField.getText());
 					commitEdit(getItem());
-					
-					if (getItem() instanceof Path) { // renaming all the endpoints path
+
+					if (getItem() instanceof Path) { // renaming all the
+														// endpoints path
 						Item currentItem = getItem();
 						while (currentItem.hasChildren()) {
 							for (final Item child : currentItem.getChildren()) {
