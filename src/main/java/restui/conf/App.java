b9c6e263f6e3f9e44b7149ddd4d26bc0450e143f
Olivier MATHE <omathe@free.fr>
2019-12-22 20:46:19 +0100

    constant refactoring

--- src/main/java/restui/conf/App.java
+++ src/main/java/restui/conf/App.java
@@ -5,18 +5,19 @@ public interface App {
 	String TITLE = "RestUI";
 
 	// FXML
-	String MAIN_FXML = "/fxml/restui.fxml";
-	String PROJECT_FXML = "/fxml/project.fxml";
-	String ENDPOINT_FXML = "/fxml/endpoint.fxml";
-	String REQUEST_BODY_FXML = "/fxml/requestBody.fxml";
-	String TEST_FXML = "/fxml/test.fxml";
+	String FXML_LOCATION = "/fxml";
+	String MAIN_FXML = FXML_LOCATION + "/restui.fxml";
+	String PROJECT_FXML = FXML_LOCATION + "/project.fxml";
+	String ENDPOINT_FXML = FXML_LOCATION + "/endpoint.fxml";
+	String REQUEST_BODY_FXML = FXML_LOCATION + "/requestBody.fxml";
+	String TEST_FXML = FXML_LOCATION + "/test.fxml";
 
 	String HOME = "restui";
 	String APLICATION_FILE = getApplicationHome() + "/" + "application.xml";
 	String STYLE_LOCATION = "/style";
 	String ICON = STYLE_LOCATION + "/applicationIcon.png";
 	String DEFAULT_STYLE_URI = "file:///" + getApplicationHome() + STYLE_LOCATION + "/default/stylesheet.css";
-	String DARK_STYLE_URI = "file://" + getApplicationHome() + STYLE_LOCATION + "/dark/stylesheet.css";
+	String DARK_STYLE_URI = "file:///" + getApplicationHome() + STYLE_LOCATION + "/dark/stylesheet.css";
 
 	static String getApplicationHome() {
 
