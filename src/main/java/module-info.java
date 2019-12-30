b87ec9ad09392a05842280c2c3d17413ae7db647
oma <oma@KLK498.klksi.local>
2019-12-30 17:44:08 +0100

    modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,11 +10,12 @@ module fr.omathe.restui {
 	requires jackson.databind;
 
 	opens fr.omathe.restui.gui to javafx.graphics;
-	//opens fr.omathe.restui.controller to javafx.fxml;
-
 	opens fr.omathe.restui.controller;
+	opens fr.omathe.restui.model;
+	
 	//opens fxml to javafx.graphics;
 
+	// exported package
 	exports fr.omathe.restui.gui;
 
 }
